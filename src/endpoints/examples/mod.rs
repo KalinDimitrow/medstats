7f5e167d10f96f5184286a7abc5123fb90bd606b
kalindimitrov <kalindimitrovv@gmail.com>
2024-06-30 23:07:44 +0300

    Add cors example for allowing any cross origin

--- src/endpoints/examples/mod.rs
+++ src/endpoints/examples/mod.rs
@@ -7,12 +7,18 @@ use axum::{
     routing::{get, post},
     Router,
 };
+use axum::http::Method;
+use tower_http::cors::{CorsLayer, Any};
 
 pub fn router() -> Router {
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
     Router::new()
         .route("/json", get(validate_json::get_json))
         .route("/validate_json", get(validate_json::validated_json))
         .route("/path/:some_id", get(path_extraction::path_extraction))
         .route("/request_params", get(request_params::request_params))
         .route("/get_headers", get(headers::headers))
+        .layer(cors)
 }
