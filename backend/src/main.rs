3f59561774d625cd9355a85b5b8fdda60bddb2fe
KalinDimitrow <kalindimitrovv@gmail.com>
2024-08-03 10:55:28 +0300

    add logging and put some constants in .env file

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,14 @@
 use actix_web::HttpServer;
+use dotenv::dotenv;
 use listenfd::ListenFd;
+use log::info;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
+    dotenv().ok();
+    env_logger::init();
+
     let pool = backend::configure_database().await?;
 
     let mut server = HttpServer::new(move || backend::create_app(pool.clone()));
@@ -10,8 +16,14 @@ async fn main() -> anyhow::Result<()> {
 
     server = match listenfd.take_tcp_listener(0)? {
         Some(listener) => server.listen(listener)?,
-        None => server.bind("localhost:3000")?,
+        None => {
+            let host = env::var("HOST").expect("Host not set");
+            let port = env::var("PORT").expect("Port not set");
+            server.bind(format!("{}:{}", host, port))?
+        }
     };
+
+    info!("Starting server");
     server.run().await?;
     Ok(())
 }
